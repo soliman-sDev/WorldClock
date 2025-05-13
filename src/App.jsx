c7f12be9fee4b17887d2e03d771d026be7f56b4b
soliman-sDev <202615672+soliman-sDev@users.noreply.github.com>
2025-05-13 16:32:00 +0300

    web analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { DarkModeProvider } from "./components/Provider/AppContext";
 import Wrapper from "./components/Wrapper/Wrapper";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <DarkModeProvider>
       <Wrapper />
+      <Analytics />
     </DarkModeProvider>
   );
 }
